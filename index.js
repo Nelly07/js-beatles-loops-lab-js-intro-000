ed2ce21c4d813691c7b886699adcc2b72ec71c05
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-08 19:14:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 function theBeatlesPlay(musicians, instruments){
-  var empty=[];
-  var musicians=["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var instruments=["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  var allmusic=[];
   for (var i = 0; i < musicians.length; i++){
-    for (var j = 0; j < instruments.length; j++){
-      if (i==j);
-    }
-    console.log(musicians+'plays'+instruments);
+    allmusic.push(musicians[i]+'plays'+instruments[i]);
   }
+  return allmusic;
 }
+
 
 
 
