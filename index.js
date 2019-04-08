623ffc155b538cc577d788de6c7e80d4e86359fa
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-08 19:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,3 @@ function theBeatlesPlay(musicians, instruments){
 
 
 
-
-//for (var i = 1; i < 100; i++) {
-//  console.log("Hello World the " + i + " time");
-
-
-
-
-//var musicians=["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-//var instruments=["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
