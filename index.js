4ed5f2af1379e8237409e754714341557a3fe15f
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-08 19:12:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var empty=[];
+  var musicians=["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments=["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  for (var i = 0; i < musicians.length; i++){
+    for (var j = 0; j < instruments.length; j++){
+      if (i==j);
+    }
+    console.log(musicians+'plays'+instruments);
+  }
+}
+
+
+
+
+
+
+
+
+
+//for (var i = 1; i < 100; i++) {
+//  console.log("Hello World the " + i + " time");
+
+
+
+
+//var musicians=["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+//var instruments=["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
