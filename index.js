7be2a1b1f10f4e14ee0f6c762231d582d6081fa5
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-08 19:15:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var allmusic=[];
   for (var i = 0; i < musicians.length; i++){
-    allmusic.push(musicians[i] + 'plays' + instruments[i]);
+    allmusic.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return allmusic;
 }
